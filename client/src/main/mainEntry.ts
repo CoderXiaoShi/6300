886d0333534762b4644323e7df8455bfa24ee8f4
石兴龙 <1003865572@qq.com>
2023-10-02 22:51:55 +0800

    Update mainEntry.ts

--- client/src/main/mainEntry.ts
+++ client/src/main/mainEntry.ts
@@ -4,21 +4,11 @@ let mainWindow: BrowserWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    // transparent: false,
     width: 620,
     height: 620,
-    // frame: false,
-    // skipTaskbar: false,
     center: true,
-    // minWidth: 650,
     webPreferences: {
       webSecurity: false,
-      // webviewTag: true,
-      // spellcheck: false,
-      // nodeIntegration: true,
-      // allowRunningInsecureContent: true,
-      // backgroundThrottling: true, // 提高最小化页面的性能
-      // contextIsolation: false,
     },
   });
   mainWindow.webContents.openDevTools()
